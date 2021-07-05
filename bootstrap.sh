f7ecdab4b366f293afe2675442bfab1486afe719
Bassam Ismail <skippednote@gmail.com>
2021-07-05 12:46:53 +0530

    Switch to minimal fish

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,39 +1,14 @@
 #!/bin/bash
 
 # create directories
-mkdir $HOME/.zsh
-mkdir -p $HOME/.config/nvim
-mkdir -p $HOME/.config/kitty
-
-# zsh plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.zsh/plugins/zsh-autosuggestions
-git clone https://github.com/lukechilds/zsh-nvm.git $HOME/.zsh/plugins/zsh-nvm
-git clone https://github.com/zsh-users/zsh-history-substring-search $HOME/.zsh/plugins/zsh-history-substring-search
-git clone https://github.com/wfxr/forgit.git $HOME/.zsh/plugins/forgit
-
-# kitty themes
-git clone --depth 1 git@github.com:dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
+mkdir -p $HOME/.config/fish
 
 # symlink files to $HOME
-ln -sfn $PWD/zshrc $HOME/.zshrc
+ln -sfn $PWD/config.fish $HOME/.config/fish/config.fish
 ln -sfn $PWD/gitconfig $HOME/.gitconfig
-ln -sfn $PWD/init.vim $HOME/.config/nvim/init.vim
-ln -sfn $PWD/tmux.conf $HOME/.tmux.conf
-ln -sfn $PWD/kitty.conf $HOME/.config/kitty/kitty.conf
-ln -sfn $HOME/.config/kitty/kitty-themes/themes/gruvbox_dark.conf $HOME/.config/kitty/theme.conf
 
-# linux
-if [[ $OSTYPE == *"darwin"* ]]; then
-  ln -sfn $PWD/Xresources $HOME/.Xresources
-  ln -sfn $PWD/Xmodmap $HOME/.Xmodmap
-fi
-
-# macos
-if [[ $OSTYPE == *"darwin"* ]]; then
-  defaults write -g KeyRepeat -int 1
-  defaults write -g InitialKeyRepeat -int 10
-  defaults write com.apple.dock persistent-apps -array
-  defaults write com.apple.dock persistent-others -array
-  killall Dock
-fi
+# Install fisher and plugins
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+fisher install jorgebucaran/fisher
+fisher install jorgebucaran/nvm.fish
+fisher install jethrokuan/z
--- config.fish
+++ config.fish
@@ -1,3 +1,5 @@
+starship init fish | source
+
 alias l="exa -lh --git"
 alias a="exa -lha --git"
 alias c='clear'
@@ -8,27 +10,31 @@ alias ls="exa --git"
 alias tree="ls --tree"
 alias cat="bat"
 alias k="kubectl"
-alias g="git"
-
-if [[ $OSTYPE == *"linux"* ]]; then
-  alias pbcopy="xsel --clipboard"
-  alias pbpaste="xsel --clipboard"
-  alias open="xdg-open"
-fi
-
 alias d="cd ~/code/personal/dotfiles"
-alias j='v ~/Documents/journal/$(date +"%d-%m-%Y")".md"'
 alias reload="source ~/.zshrc"
 alias path='echo $PATH | tr ":" "\n"'
-
-
-# ssh keys
 alias genkeys='ssh-keygen -t ed25519 -C "skippednote@gmail.com" '
 alias copykeys='cat ~/.ssh/id_ed25519.pub | pbcopy'
-
 alias vu='vagrant up'
 alias vs='vagrant ssh'
 alias vh='vagrant halt'
 alias vp='vagrant provision'
+alias yt='docker run --rm -u (id -u):(id -g) -v (PWD):/data vimagick/youtube-dl'
+alias g="git"
+alias grr="git remote remove"
+alias gra="git remote add"
+alias createdrupal="composer create-project drupal/recommended-project"
 
-alias yt='docker run --rm -u $(id -u):$(id -g) -v $PWD:/data vimagick/youtube-dl'
+function mcd
+  mkdir $argv[1]
+  cd $argv[1]
+end
+
+function skipCyPup
+  set -gx CYPRESS_INSTALL_BINARY 0
+  set -gx PUPPETEER_SKIP_CHROMIUM_DOWNLOAD 0
+end
+
+
+set -gx BAT_THEME Dracula
+set -g fish_user_paths /usr/local/nvim/bin $HOME/.platformsh/bin $fish_user_paths
